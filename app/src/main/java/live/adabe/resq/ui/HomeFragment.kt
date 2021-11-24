7fbd91c7cd9a22bc50278131b79e268aac40324f
Adabe Echaim <adabeechaim@gmail.com>
2021-11-24 21:03:30 +0100

    setup navigation

--- app/src/main/java/live/adabe/resq/ui/HomeFragment.kt
+++ app/src/main/java/live/adabe/resq/ui/HomeFragment.kt
@@ -6,27 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
 import live.adabe.resq.R
+import live.adabe.resq.databinding.HomeFragmentBinding
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
     private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    ): View {
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        return binding.root
     }
 
 }
