652db4782c9ab4b75ded4783a5a3635183baafa2
Adabe <adabeechaim@gmail.com>
2021-12-20 22:24:16 +0100

    fix permissions for handheld and wear

--- app/src/main/java/live/adabe/resq/MainActivity.kt
+++ app/src/main/java/live/adabe/resq/MainActivity.kt
@@ -1,7 +1,6 @@
 package live.adabe.resq
 
 import android.Manifest
-import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +9,11 @@ import dagger.hilt.android.AndroidEntryPoint
 import live.adabe.resq.databinding.ActivityMainBinding
 import live.adabe.resq.navigation.NavigationService
 import live.adabe.resq.util.Preferences
+import live.adabe.resq.util.hasPermissions
 import javax.inject.Inject
 
+
+@Suppress("PrivatePropertyName")
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,16 +23,19 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var navigationService: NavigationService
+    private val REQUEST_PERMISSION_SETTING = 101
 
-    val SMS_PERMISSION_CONSTANT = 100
-    val LOCATION_PERMISSION_CONSTANT = 102
-    val REQUEST_PERMISSION_SETTING = 101
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION, Manifest
+            .permission.SEND_SMS, Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navigationService.attachToActivity(this)
+        checkAndResetPermissions()
         preferences.run {
             setIsLocationPermissionGranted(
                 ActivityCompat.checkSelfPermission(
@@ -39,32 +44,52 @@ class MainActivity : AppCompatActivity() {
                 ) == PackageManager.PERMISSION_GRANTED
             )
 
-            setIsSmsPermissionGranted(ActivityCompat.checkSelfPermission(
-                this@MainActivity,
-                Manifest.permission.SEND_SMS
-            ) == PackageManager.PERMISSION_GRANTED)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (!preferences.getIsLocationPermissionGranted()) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                LOCATION_PERMISSION_CONSTANT
+            setIsSmsPermissionGranted(
+                ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.SEND_SMS
+                ) == PackageManager.PERMISSION_GRANTED
             )
         }
-        if (!preferences.getIsSmsPermissionGranted()){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), SMS_PERMISSION_CONSTANT)
-        }
 
-        if (!preferences.getIsRegistered()){
-            navigationService.openSignupScreen()
-        }else{
-            navigationService.openHomeScreen()
+    }
+
+    private fun checkAndResetPermissions() {
+        if (!hasPermissions(this, *permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSION_SETTING)
+        } else {
+            if (!preferences.getIsRegistered()) {
+                navigationService.openSignupScreen()
+            } else {
+                navigationService.openHomeScreen()
+            }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSION_SETTING && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            permissions.forEach { permission ->
+                when (permission) {
+                    Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                    }
+                    Manifest.permission.SEND_SMS -> {
+                    }
+                }
+            }
+            if (!preferences.getIsRegistered()) {
+                navigationService.openSignupScreen()
+            } else {
+                navigationService.openHomeScreen()
+            }
+        }
+    }
+
+
+
 
     override fun onDestroy() {
         super.onDestroy()
--- resqwears/src/main/java/live/adabe/resqwears/MainActivity.kt
+++ resqwears/src/main/java/live/adabe/resqwears/MainActivity.kt
@@ -1,12 +1,24 @@
 package live.adabe.resqwears
 
+import android.Manifest
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 import live.adabe.resqwears.databinding.ActivityMainBinding
+import live.adabe.resqwears.util.hasPermissions
 
 class MainActivity : Activity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val PERMISSION_CODE = 100
+
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION, Manifest
+            .permission.ACCESS_COARSE_LOCATION, Manifest.permission.BODY_SENSORS
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +26,19 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (!hasPermissions(this, *permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_CODE)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }){
+            Snackbar.make(binding.root, "Permissions granted", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
